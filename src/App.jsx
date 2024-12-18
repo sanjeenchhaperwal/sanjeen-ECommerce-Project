005feea50b1ba905e9f616ed8764bc5739c36f15
sanjeenchhaperwal <sanjeen0chhaperwal@gmail.com>
2024-12-18 15:36:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import AOS from "aos"
+import "aos/dist/aos.css";
+import { useEffect,useState } from "react";
+import Banner from "./Components/banner/Banner";
+
+function App() {
+ 
+ 
+  
+  return (
+    <>
+      <div>
+        <Banner />
+      </div>
+    </>
+  )
+}
+
+export default App
